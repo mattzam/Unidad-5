3af1639a9d7d070805453190801746d3916a1fff
Matthew Zamudio <143620932+mattzam@users.noreply.github.com>
2023-11-09 21:32:26 -0600

    Add files via upload

---
+++ MetodosU4.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metodosu4;
+/**
+ *
+ * @author expec
+ */
+public class MetodosU4 {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        int resultadoSuma = suma(5, 3);
+        System.out.println("Suma: " + resultadoSuma);
+
+        int resultadoResta = resta(5, 3);
+        System.out.println("Resta: " + resultadoResta);
+
+        int resultadoProducto = producto(5, 3);
+        System.out.println("Producto: " + resultadoProducto);
+
+        int resultadoDivision = division(6, 2);
+        System.out.println("División: " + resultadoDivision);
+    }
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+    public static int resta(int a, int b) {
+        return a - b;
+    }
+    public static int producto(int a, int b) {
+        return a * b;
+    }
+    public static int division(int a, int b) {
+            return a / b;
+        }
+}
